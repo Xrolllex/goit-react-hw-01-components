f0fbcd432f50c98bd2bec0860e27c243a999e2d7
Xrolllex <karolkaczanowski@wp.pl>
2023-11-19 21:35:58 +0100

    commit

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -8,7 +8,7 @@ const Profile = ({avatar, username, tag, location, stats }) => (
       <div className="description">
         <img
           src={avatar}
-          alt="user-photo"
+          
           className="avatar"
         />
         <p className="name">{username}</p>
